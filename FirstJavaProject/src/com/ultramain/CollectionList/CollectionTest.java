21bf5f16bbd16a491a135173466f8a47135438d9
ELCOT <ELCOT@HP>
2021-09-01 15:36:02 +0530

    Sorted the ArrayList

--- FirstJavaProject/src/com/ultramain/CollectionList/CollectionTest.java
+++ FirstJavaProject/src/com/ultramain/CollectionList/CollectionTest.java
@@ -1,22 +1,27 @@
 package com.ultramain.CollectionList;
 
-import java.awt.List;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 class ListExample{
 	public void listTest(){
-		int myarr[]=new int[10];
+		Integer myarr[]=new Integer[10];
 		for(int i=0;i<10;i++){
 			myarr[i]=new Random().nextInt(50);
 		}
 		for(int res : myarr){
 			System.out.println(res);
 		}
-		ArrayList<Integer> numList=new ArrayList<Integer>();
-		numList= (ArrayList) Arrays.asList(myarr);
 		
+		List<Integer> numList;
+		numList=Arrays.asList(myarr);
+		Collections.sort(numList);
+		System.out.println("After sorting");
+		for(int res : numList){
+			System.out.println(res);
+		}
 		
 	}
 }
